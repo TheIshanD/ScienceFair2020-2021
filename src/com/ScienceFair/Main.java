21117337b27ff0310cf18c3f9f7fc52149586399
TheIshanD <56279050+TheIshanD@users.noreply.github.com>
2021-01-01 14:32:52 -0600

    changing file paths

--- src/com/ScienceFair/Main.java
+++ src/com/ScienceFair/Main.java
@@ -6,12 +6,11 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File inputFile = new File("travis-addresses-county.geojson");
-        File outputFile = new File("Home-Addresses-Travis-County-Cleaned.dat");
+        File inputFile = new File("dataFiles//travis-addresses-county.geojson");
+        File outputFile = new File("dataFiles//Home-Addresses-Travis-County-Cleaned.dat");
         DataCleanser cleanser = new DataCleanser(inputFile, outputFile);
 
         cleanser.postcode = true;
-        cleanser.streetName = true;
 
         cleanser.cleanAddresses();
     }
